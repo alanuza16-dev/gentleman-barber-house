87d238598ed8d70da09dcf5d1bdc84a1e90a89fa
alanuza16-dev <alanuza16@gmail.com>
2026-04-17 22:52:51 -0600

    Remove unused Lanzah starter files

--- gui/gui_index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include get_template_directory() . '/gui/gui_nav.php';
-include get_template_directory() . '/gui/gui_masthead.php';
-include get_template_directory() . '/gui/gui_about.php';
-include get_template_directory() . '/gui/gui_projects.php';
-include get_template_directory() . '/gui/gui_signup.php';
-include get_template_directory() . '/gui/gui_contact.php';
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header();
-
-if (file_exists(get_template_directory() . '/gui/gui_index.php')) {
-    include get_template_directory() . '/gui/gui_index.php';
-}
-
-get_footer();
